53a35f65b92c80e349ddc34f0d61e5126a6a3589
Nick Looney <nlooney10@aim.com>
2015-11-01 15:24:50 -0500

    Initial Commit

---
+++ Photo Search Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Search Example
+//
+//  Created by Nicholas Looney on 11/1/15.
+//  Copyright © 2015 Nicholas Looney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
